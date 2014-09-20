463168eeb915cfc7c6499c3dfedc5b569bf0493d
calen-legaspi <calen@orangeandbronze.com>
2014-09-20 12:27:49 +0800

    Formattd the Javadoc a bit.

--- src/main/java/com/orangeandbronze/schoolreg/domain/Entity.java
+++ src/main/java/com/orangeandbronze/schoolreg/domain/Entity.java
@@ -1,13 +1,18 @@
 package com.orangeandbronze.schoolreg.domain;
 
 /**
- * This implements the <a href="http://martinfowler.com/eaaCatalog/layerSupertype.html">Layer Supertype</a> pattern for all entities in this domain model. Contains the private
- * key that maps each entity to a database row.
+ * This implements the 
+ * <a href="http://martinfowler.com/eaaCatalog/layerSupertype.html">
+ * Layer Supertype
+ * </a> pattern for all entities in this domain model. Contains the
+ * private key that maps each entity to a database row.
  * 
- * Notice that it's private and final! This means it has to be set by Reflection! This is how Hibernate does it. Implement the JDBC DAOs to set and get this field by Reflection.
+ * Notice that it's private and final! This means it has to be set by
+ * Reflection! This is how Hibernate does it. Implement the JDBC DAOs to set and
+ * get this field by Reflection.
  **/
 public abstract class Entity {
-	
+
 	/** Should be set by reflection. **/
 	private final Long privateKey = null;
 
