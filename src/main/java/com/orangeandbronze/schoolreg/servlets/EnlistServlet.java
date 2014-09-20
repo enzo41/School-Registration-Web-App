70b9417cecb194e5c3e7041d07d5e476e52d0ef6
calen-legaspi <calen@orangeandbronze.com>
2014-09-20 18:29:33 +0800

    Improved formatting of EnlistServlet placeholder response.

--- src/main/java/com/orangeandbronze/schoolreg/servlets/EnlistServlet.java
+++ src/main/java/com/orangeandbronze/schoolreg/servlets/EnlistServlet.java
@@ -17,19 +17,26 @@ import com.orangeandbronze.schoolreg.service.EnlistmentService;
 @WebServlet("/enlist")
 public class EnlistServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	private EnlistmentService service = new EnlistmentService();
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		String[] sectionNumbers = request.getParameterValues("sectionNumber");
-		
+
 		// placeholder code
 		response.setContentType("text/html");
 		PrintWriter w = response.getWriter();
-		w.println("<p>You have chosen to enlist in the following section #s: " + sectionNumbers);
+		w.println("<p>You have chosen to enlist in the following section #s:");
+		w.println("<ul>");
+		for (String num : sectionNumbers) {
+			w.println("<li>" + num);
+		}
+		w.println("</ul>");
 		w.println("<p>Now get to work and finish this machine problem!");
 		w.flush();
 	}
