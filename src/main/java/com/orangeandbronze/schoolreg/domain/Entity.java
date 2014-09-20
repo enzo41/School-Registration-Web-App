aa1e3166a0131a2e9b8ad929e75a50d356d885f9
calen-legaspi <calen@orangeandbronze.com>
2014-09-20 12:26:21 +0800

    Added explanation on Layer Supertype pattern.

--- src/main/java/com/orangeandbronze/schoolreg/domain/Entity.java
+++ src/main/java/com/orangeandbronze/schoolreg/domain/Entity.java
@@ -1,8 +1,10 @@
 package com.orangeandbronze.schoolreg.domain;
 
 /**
- * Layer Supertype for all entities in this domain model. Contains the private
+ * This implements the <a href="http://martinfowler.com/eaaCatalog/layerSupertype.html">Layer Supertype</a> pattern for all entities in this domain model. Contains the private
  * key that maps each entity to a database row.
+ * 
+ * Notice that it's private and final! This means it has to be set by Reflection! This is how Hibernate does it. Implement the JDBC DAOs to set and get this field by Reflection.
  **/
 public abstract class Entity {
 	
