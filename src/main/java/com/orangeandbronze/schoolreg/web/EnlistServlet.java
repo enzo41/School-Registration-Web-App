27ce047a925d8e9003b242f1e191ab5a13fcc286
calen-legaspi <calen@orangeandbronze.com>
2014-09-20 13:24:48 +0800

    Added EnlistmentService as member of EnlistmentServlet. Notice I eschew interfaces here!

--- src/main/java/com/orangeandbronze/schoolreg/web/EnlistServlet.java
+++ src/main/java/com/orangeandbronze/schoolreg/web/EnlistServlet.java
@@ -1,11 +1,14 @@
 package com.orangeandbronze.schoolreg.web;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.orangeandbronze.schoolreg.service.EnlistmentService;
 
 /**
  * Servlet implementation class EnlistServlet
@@ -13,6 +16,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/enlist")
 public class EnlistServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private EnlistmentService service = new EnlistmentService();
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
