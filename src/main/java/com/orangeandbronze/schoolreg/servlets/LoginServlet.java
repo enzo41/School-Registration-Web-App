43b6d350afe0eac95cd9717e700bbbc1d929f540
calen-legaspi <calen@orangeandbronze.com>
2014-09-20 16:13:14 +0800

    Added LoginService to AuthenticationFilter as a field.

--- src/main/java/com/orangeandbronze/schoolreg/servlets/LoginServlet.java
+++ src/main/java/com/orangeandbronze/schoolreg/servlets/LoginServlet.java
@@ -1,11 +1,14 @@
 package com.orangeandbronze.schoolreg.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.orangeandbronze.schoolreg.service.LoginService;
 
 /**
  * Servlet implementation class LoginServlet
@@ -13,12 +16,14 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private LoginService service = new LoginService();
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String studentNumber = request.getParameter("studentNumber");
+		String userId = request.getParameter("userId");
 	}
 
 }
