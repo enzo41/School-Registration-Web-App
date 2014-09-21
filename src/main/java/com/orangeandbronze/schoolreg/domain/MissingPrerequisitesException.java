70e2bf91bc63fda9bc8befc4d11b3fc0e1029d34
calen-legaspi <calen@orangeandbronze.com>
2014-09-21 12:38:05 +0800

    Made constructors of Exceptions package-private, since they shouldn't be instantiated outside of the package.

--- src/main/java/com/orangeandbronze/schoolreg/domain/MissingPrerequisitesException.java
+++ src/main/java/com/orangeandbronze/schoolreg/domain/MissingPrerequisitesException.java
@@ -3,19 +3,19 @@ package com.orangeandbronze.schoolreg.domain;
 public class MissingPrerequisitesException extends RuntimeException {
 
 
-	public MissingPrerequisitesException(String message) {
+	MissingPrerequisitesException(String message) {
 		super(message);
 	}
 
-	public MissingPrerequisitesException(Throwable cause) {
+	MissingPrerequisitesException(Throwable cause) {
 		super(cause);
 	}
 
-	public MissingPrerequisitesException(String message, Throwable cause) {
+	MissingPrerequisitesException(String message, Throwable cause) {
 		super(message, cause);
 	}
 
-	public MissingPrerequisitesException(String message, Throwable cause,
+	MissingPrerequisitesException(String message, Throwable cause,
 			boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
 	}
