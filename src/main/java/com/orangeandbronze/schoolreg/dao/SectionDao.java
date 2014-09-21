a4af7a18cf5b424204f3f6bb9c9336e73b2b8a01
calen-legaspi <calen@orangeandbronze.com>
2014-09-21 19:39:32 +0800

    Added getAll stub method to SectionDao.

--- src/main/java/com/orangeandbronze/schoolreg/dao/SectionDao.java
+++ src/main/java/com/orangeandbronze/schoolreg/dao/SectionDao.java
@@ -1,11 +1,32 @@
 package com.orangeandbronze.schoolreg.dao;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.orangeandbronze.schoolreg.domain.Days;
+import com.orangeandbronze.schoolreg.domain.Period;
+import com.orangeandbronze.schoolreg.domain.Schedule;
 import com.orangeandbronze.schoolreg.domain.Section;
+import com.orangeandbronze.schoolreg.domain.Subject;
 
 public class SectionDao {
 
 	public Section getById(String sectionNumber) {
-		return new Section(sectionNumber, null); // TODO Just a stub; implement actual as JDBC
+		return new Section(sectionNumber, null); // TODO Just a stub; implement
+													// actual as JDBC
+	}
+
+	public Set<Section> getAll() {
+		// TODO Just a stub; implement actual as JDBC
+		Section aaa111 = new Section("AAA111", new Subject("MATH53"), new Schedule(Days.MTH, Period.AM10));
+		Section bbb222 = new Section("BBB222", new Subject("COM1"));
+		Section ccc333 = new Section("CCC333", new Subject("CS11"));
+		Section ddd444 = new Section("DDD444", new Subject("PHILO1"), new Schedule(Days.TF, Period.PM4));
+		Section eee555 = new Section("EEE555", new Subject("CS11")); 
+		Section zzz000 = new Section("ZZZ000", new Subject("CHEM11"), new Schedule(Days.TF, Period.PM4));
+		return new HashSet<Section>() {{
+				add(aaa111); add(bbb222); add(ccc333); add(ddd444); add(eee555); add(zzz000);
+		}};
 	}
 
 }
