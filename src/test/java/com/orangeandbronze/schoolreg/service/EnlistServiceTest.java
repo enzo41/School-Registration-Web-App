171425cf937bb9a7b3009fb09d1607da51bad882
calen-legaspi <calen@orangeandbronze.com>
2014-09-21 19:35:12 +0800

    Changed variable name.

--- src/test/java/com/orangeandbronze/schoolreg/service/EnlistServiceTest.java
+++ src/test/java/com/orangeandbronze/schoolreg/service/EnlistServiceTest.java
@@ -25,10 +25,10 @@ public class EnlistServiceTest {
 		Section alreadyEnlisted = new Section("ZZZ000", new Subject("CHEM11"), new Schedule(Days.TF, Period.PM4));
 		Enrollment currentEnrollment = new Enrollment(143, student, Term.getCurrent(), new HashSet<Section>() {{ add(alreadyEnlisted); }});
 		// three sections should pass
-		Section bbb111 = new Section(sectionNumbers[1], new Subject("COM1"));
+		Section bbb222 = new Section(sectionNumbers[1], new Subject("COM1"));
 		Section ccc333 = new Section(sectionNumbers[2], new Subject("CS11"));
 		Section eee555 = new Section(sectionNumbers[4], new Subject("CS11"));
-		Set<Section> successfullyEnlisted = new HashSet<Section>() {{ add(bbb111); add(ccc333); add(eee555); }}; 
+		Set<Section> successfullyEnlisted = new HashSet<Section>() {{ add(bbb222); add(ccc333); add(eee555); }}; 
 		
 		// Map of failed enlistments
 		Map<Section, String> failedToEnlist = new HashMap<>();
@@ -55,7 +55,7 @@ public class EnlistServiceTest {
 		when(studentDao.getById(studentNumber)).thenReturn(student);
 		SectionDao sectionDao = mock(SectionDao.class);
 		when(sectionDao.getById(sectionNumbers[0])).thenReturn(aaa111);
-		when(sectionDao.getById(sectionNumbers[1])).thenReturn(bbb111);
+		when(sectionDao.getById(sectionNumbers[1])).thenReturn(bbb222);
 		when(sectionDao.getById(sectionNumbers[2])).thenReturn(ccc333);
 		when(sectionDao.getById(sectionNumbers[3])).thenReturn(ddd444);
 		when(sectionDao.getById(sectionNumbers[4])).thenReturn(eee555);
