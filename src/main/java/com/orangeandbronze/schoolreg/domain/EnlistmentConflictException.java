2c4d59782d0bb8b6b5bd0e7f89e936d2a0b2fdae
calen-legaspi <calen@orangeandbronze.com>
2014-09-21 12:40:28 +0800

    Forgot to save EnlistmentConflictException in previous commit.

--- src/main/java/com/orangeandbronze/schoolreg/domain/EnlistmentConflictException.java
+++ src/main/java/com/orangeandbronze/schoolreg/domain/EnlistmentConflictException.java
@@ -3,19 +3,19 @@ package com.orangeandbronze.schoolreg.domain;
 public class EnlistmentConflictException extends RuntimeException {
 
 
-	public EnlistmentConflictException(String message) {
+	EnlistmentConflictException(String message) {
 		super(message);
 	}
 
-	public EnlistmentConflictException(Throwable cause) {
+	EnlistmentConflictException(Throwable cause) {
 		super(cause);
 	}
 
-	public EnlistmentConflictException(String message, Throwable cause) {
+	EnlistmentConflictException(String message, Throwable cause) {
 		super(message, cause);
 	}
 
-	public EnlistmentConflictException(String message, Throwable cause,
+	EnlistmentConflictException(String message, Throwable cause,
 			boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
 	}
