6d9c9564c68beac2d346e8a26184013be822334b
calen-legaspi <calen@orangeandbronze.com>
2014-09-22 17:30:32 +0800

    Simplified toString

--- src/main/java/com/orangeandbronze/schoolreg/domain/Schedule.java
+++ src/main/java/com/orangeandbronze/schoolreg/domain/Schedule.java
@@ -6,7 +6,7 @@ public class Schedule {
 
 		@Override
 		public String toString() {
-			return "Schedule [TBA]";
+			return "TBA";
 		}
 	};
 
@@ -53,7 +53,7 @@ public class Schedule {
 
 	@Override
 	public String toString() {
-		return "Schedule [" + days + " " + period + "]";
+		return days + " " + period;
 	}
 
 }
