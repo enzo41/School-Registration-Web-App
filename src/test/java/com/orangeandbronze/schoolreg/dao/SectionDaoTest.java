a8fb81013a2c227c2136cd36c8c7ce1ed4ffb5d1
calen-legaspi <calen@orangeandbronze.com>
2014-09-22 18:58:12 +0800

    Stub for SectionDaoTest & its dataset.

---
+++ src/test/java/com/orangeandbronze/schoolreg/dao/SectionDaoTest.java
@@ -0,0 +1,14 @@
+package com.orangeandbronze.schoolreg.dao;
+
+import org.dbunit.DBTestCase;
+import org.dbunit.dataset.IDataSet;
+
+public class SectionDaoTest extends DBTestCase {
+
+	@Override
+	protected IDataSet getDataSet() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
