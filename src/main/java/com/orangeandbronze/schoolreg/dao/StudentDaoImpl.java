09bed36b6f27f06250f0bdce27ec61da812cd18e
enzo <karl.abiog@misys.com>
2014-09-23 18:22:26 +0800

    Created for getting all student numbers.

---
+++ src/main/java/com/orangeandbronze/schoolreg/dao/StudentDaoImpl.java
@@ -0,0 +1,43 @@
+package com.orangeandbronze.schoolreg.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.orangeandbronze.schoolreg.domain.Student;
+import com.orangeandbronze.schoolreg.domain.Subject;
+import com.orangeandbronze.schoolreg.dao.StudentDao;
+
+public class StudentDaoImpl extends Dao implements StudentDao {
+	
+	// Getting the list of Students for the Login
+	
+	public List<Student> getAllStudents() {
+		List<Student> students = null;
+
+		String sql = "select pk, student_number from students";
+		List<Student> studentList = new ArrayList<>();
+		try (Connection conn = getConnection()) {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				Integer studentId = rs.getInt("subject_id");
+				Student student = new Student(studentId);
+				studentList.add(student);
+			}
+		} catch (SQLException e) {
+			throw new DataAccessException("Something happend while trying to fetch Section data", e);
+		}
+		return studentList;
+	}
+	
+	public Student getStudentById(){
+		return null;
+	}
+
+}
+
