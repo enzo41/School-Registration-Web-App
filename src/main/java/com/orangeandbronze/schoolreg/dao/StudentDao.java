b8c1ef41d082bdb0502416df772b2d6852671b26
enzo <karl.abiog@misys.com>
2014-09-24 11:27:11 +0800

    Updated Student DAO as interface

--- src/main/java/com/orangeandbronze/schoolreg/dao/StudentDao.java
+++ src/main/java/com/orangeandbronze/schoolreg/dao/StudentDao.java
@@ -4,17 +4,19 @@ import java.util.List;
 
 import com.orangeandbronze.schoolreg.domain.Student;
 
-/*public interface StudentDaoS {
+public interface StudentDao {
 	
 	List<Student> getAllStudents();
-	Student getById();
+	Student getById(int studentNumber);
+	List<Integer> getStudentsInt();
 
-}*/
+}
 
+/*
 public class StudentDao extends Dao {
 	
 	public Student getById(int studentId) {
 		return new Student(studentId); // TODO Just a stub; implement actual as JDBC
 	}
 
-}
+}*/
