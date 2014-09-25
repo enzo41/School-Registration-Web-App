d12fef2a2723f2b165b55492c7877ed65c6655c5
enzo <karl.abiog@misys.com>
2014-09-25 15:58:20 +0800

    removed unnecessary lines that may cause error

--- src/main/java/com/orangeandbronze/schoolreg/servlets/EnlistServlet.java
+++ src/main/java/com/orangeandbronze/schoolreg/servlets/EnlistServlet.java
@@ -38,8 +38,6 @@ public class EnlistServlet extends HttpServlet {
 		// added try and catch for null data or no section selected to loop in showSections
 		catch(NullPointerException e){
 		session.setAttribute("noSection", true);
-		session.setAttribute("sampleD", true);
-		session.setAttribute("selection", sectionNumbers[0]);
 		response.sendRedirect(getServletContext().getContextPath() + "/showSections");
 		}
 	}
