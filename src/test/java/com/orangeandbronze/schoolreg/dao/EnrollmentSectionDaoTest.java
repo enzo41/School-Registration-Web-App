1b8c2f0877a794c31ad44e75ce0c85eb231e3d70
takumi-miyauchi <takumic1202@hotmail.com>
2014-09-25 18:39:10 +0800

    Applied DBUnit to EnrollmentSectionDaoTest

--- src/test/java/com/orangeandbronze/schoolreg/dao/EnrollmentSectionDaoTest.java
+++ src/test/java/com/orangeandbronze/schoolreg/dao/EnrollmentSectionDaoTest.java
@@ -1,12 +1,29 @@
 package com.orangeandbronze.schoolreg.dao;
 
-import static org.junit.Assert.*;
+import java.io.FileReader;
 
+import org.dbunit.DBTestCase;
+import org.dbunit.PropertiesBasedJdbcDatabaseTester;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.junit.Test;
 
 import com.orangeandbronze.schoolreg.domain.Term;
 
-public class EnrollmentSectionDaoTest {
+public class EnrollmentSectionDaoTest extends DBTestCase {
+	
+	public EnrollmentSectionDaoTest() {
+		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS,"com.mysql.jdbc.Driver");
+		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL,"jdbc:mysql://localhost:3306/school_registration"); 
+		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME,"root");
+		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD,"");
+	}
+
+	@Override
+	protected IDataSet getDataSet() throws Exception {
+		return new FlatXmlDataSetBuilder().build(new FileReader("src/test/EnrollmentSectionDaoTest.xml"));
+	}
+	
 	
 	@Test
 	public void testCountEnlistedSection(){
