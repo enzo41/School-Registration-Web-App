6a63945fa1d03f2bcae6a982a8f0f3f2d452d477
takumi-miyauchi <takumic1202@hotmail.com>
2014-09-25 18:13:07 +0800

    Applied DBUnit to StudentDaoTest

--- src/test/java/com/orangeandbronze/schoolreg/dao/StudentDaoTest.java
+++ src/test/java/com/orangeandbronze/schoolreg/dao/StudentDaoTest.java
@@ -1,12 +1,30 @@
 package com.orangeandbronze.schoolreg.dao;
 
-import static org.junit.Assert.*;
+import java.io.FileReader;
 
+import org.dbunit.DBTestCase;
+import org.dbunit.PropertiesBasedJdbcDatabaseTester;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.junit.Test;
 
 import com.orangeandbronze.schoolreg.domain.Student;
 
-public class StudentDaoTest {
+public class StudentDaoTest extends DBTestCase{
+	
+	public StudentDaoTest() {
+		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS,"com.mysql.jdbc.Driver");
+		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL,"jdbc:mysql://localhost:3306/school_registration"); 
+		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME,"root");
+		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD,"");
+	}
+
+	@Override
+	protected IDataSet getDataSet() throws Exception {
+		return new FlatXmlDataSetBuilder().build(new FileReader("src/test/StudentDaoTest.xml"));
+	}
+	
+	
 	
 	@Test
 	public void testGetStudentByStudentNumber(){
@@ -17,7 +35,7 @@ public class StudentDaoTest {
 		Student student = studentDao.getStudentByStudentNumber(studentNumber);
 		
 		assertEquals(1, student.getAcademicYear().intValue());
-		assertEquals("NONE", student.getShorlarshipStatus());
+		assertEquals("HALF", student.getShorlarshipStatus().toString());
 		
 	}
 	
